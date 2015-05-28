cc14a97e16d63b498b79bf82dc5fc18a95b558b1
Thomas Haddad <thomas.af.haddad@gmail.com>
2015-05-28 15:04:43 +0200

    added jade, created server, added first jade files

---
+++ server.js
@@ -0,0 +1,44 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/imagewall-dev', function (error) {
+    if (error) {
+        console.log(error);
+    }
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+    first_name: String,
+    last_name: String,
+    email: String
+});
+
+// Mongoose Model definition
+var User = mongoose.model('users', UserSchema);
+
+// Bootstrap express
+app.set('view engine', 'jade');
+
+// URLS management
+
+app.get('/', function (req, res) {
+    res.render('index', { title: 'home', message: 'Hello there!'});
+});
+
+app.get('/add', function (req, res) {
+    res.render('addImage', { title: 'add', message: 'add an image'});
+});
+
+http.listen(9000, function(){
+    console.log('listening on *:9000');
+});
